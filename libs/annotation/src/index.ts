e098dc861fffe4bcf9414925d2a4ad550e74cbff
LinBo Len <linbolen@gradii.com>
2024-07-05 11:26:07 +0800

    upate annotation

---
+++ libs/annotation/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license
+ */
+
+export * from './decorators';
+export * from './global';
+export * from './stringify';
+export * from './reflection/platform_reflection_capabilities';
+export * from './reflection/reflection';
+export * from './reflection/reflection_capabilities';
+export * from './reflection/reflector';
+export * from './reflection/types';
